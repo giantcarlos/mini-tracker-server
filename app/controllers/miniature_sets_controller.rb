e5e00fb44e2de107e83ec23eab4ffb548f1bcb91
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-09 16:46:25 -0500

    add get route handlers

--- app/controllers/miniature_sets_controller.rb
+++ app/controllers/miniature_sets_controller.rb
@@ -1,3 +1,8 @@
 class MiniatureSetsController < ApplicationController
 
+    get '/miniature_sets' do
+        miniature_sets = MiniatureSet.all
+        miniature_sets.to_json(include: :miniatures)
+    end
+
 end
--- app/controllers/miniatures_controller.rb
+++ app/controllers/miniatures_controller.rb
@@ -1,3 +1,13 @@
 class MiniaturesController < ApplicationController
 
+    get '/miniatures' do
+        miniatures = Miniature.all
+        miniatures.to_json
+    end
+
+    get '/miniatures/:id' do
+        miniature = Miniature.find_by(id: params[:id])
+        miniature.to_json
+    end
+
 end
