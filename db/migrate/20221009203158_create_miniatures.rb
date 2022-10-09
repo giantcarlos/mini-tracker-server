e9a38ecd001d5d5cad88f2495b5713af99c1a2c8
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-09 15:44:22 -0500

    create database tables

--- db/migrate/20221009203158_create_miniatures.rb
+++ db/migrate/20221009203158_create_miniatures.rb
@@ -1,4 +1,11 @@
 class CreateMiniatures < ActiveRecord::Migration[6.1]
   def change
+    create_table :miniatures do |t|
+      t.string :name
+      t.string :size
+      t.integer :units
+      t.integer :miniature_set_id
+      t.string :img_url
+    end
   end
 end
--- db/migrate/20221009203258_create_miniature_sets.rb
+++ db/migrate/20221009203258_create_miniature_sets.rb
@@ -1,4 +1,8 @@
 class CreateMiniatureSets < ActiveRecord::Migration[6.1]
   def change
+    create_table :miniature_sets do |t|
+      t.string :name
+      t.integer :year
+    end
   end
 end
