17e728b53348e5a89c44269596c0454150c01c45
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-15 18:05:03 -0500

    seed data update

--- app/controllers/miniatures_controller.rb
+++ app/controllers/miniatures_controller.rb
@@ -4,17 +4,23 @@ class MiniaturesController < ApplicationController
         Miniature.all.to_json(include: :miniature_set)
     end
 
-    get '/miniatures/new' do
-        Miniature.last(6).to_json(include: :miniature_set)
-    end
-
     get '/miniatures/:id' do
         @miniature = Miniature.find(params[:id])
         @miniature.to_json(include: :miniature_set)
     end
 
-    post '/miniatures' do
-        @miniature = Miniature.create(params)
+    get '/miniatures/new' do
+        Miniature.last(6).to_json(include: :miniature_set)
+    end
+
+    get '/miniature_sets/:miniature_set_id/miniatures"' do
+        @miniature_set = MiniatureSet.find(params[:id])
+        @miniature_set.miniatures.to_json(include: miniature_set)
+
+    post '/miniature_sets/:miniature_set_id/miniatures' do
+        @miniature_set = MiniatureSet.find(params[:id])
+        @miniature = @miniature_set.miniatures.build(params)
+        @miniature.save
         @miniature.to_json
     end
 
