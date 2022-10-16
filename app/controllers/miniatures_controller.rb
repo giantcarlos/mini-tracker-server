982d865bd1c8ea0e510c9c3d2fcddd1dd9f842d7
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-10-15 20:00:36 -0500

    add reverse miniature order

--- app/controllers/miniatures_controller.rb
+++ app/controllers/miniatures_controller.rb
@@ -1,7 +1,12 @@
 class MiniaturesController < ApplicationController
 
     get '/miniatures' do
-        Miniature.all.to_json(include: :miniature_set)
+        @miniatures = Miniature.all
+        @miniatures.reverse.to_json(include: :miniature_set)
+    end
+
+    get '/miniatures/new' do
+        Miniature.last(6).to_json(include: :miniature_set)
     end
 
     get '/miniatures/:id' do
@@ -9,16 +14,13 @@ class MiniaturesController < ApplicationController
         @miniature.to_json(include: :miniature_set)
     end
 
-    get '/miniatures/new' do
-        Miniature.last(6).to_json(include: :miniature_set)
+    get '/miniature_sets/:miniature_set_id/miniatures' do
+        @miniature_set = MiniatureSet.find(params["miniature_set_id"])
+        @miniature_set.miniatures.to_json(include: miniature_set)
     end
 
-    get '/miniature_sets/:miniature_set_id/miniatures"' do
-        @miniature_set = MiniatureSet.find(params[:id])
-        @miniature_set.miniatures.to_json(include: miniature_set)
-
     post '/miniature_sets/:miniature_set_id/miniatures' do
-        @miniature_set = MiniatureSet.find(params[:id])
+        @miniature_set = MiniatureSet.find(params["miniature_set_id"])
         @miniature = @miniature_set.miniatures.build(params)
         @miniature.save
         @miniature.to_json
